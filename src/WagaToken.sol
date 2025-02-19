80120e9c28cf7930bcec5682c3e85a54a3208b44
Manu Acho <emmanuel@earesearch.net>
2025-02-19 16:38:16 +0100

    modify token symbol

--- src/WagaToken.sol
+++ src/WagaToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract WagaToken is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("WagaToken", "WAGA") {
+    constructor() ERC20("WagaToken", "WGTN") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender); // Admin Role
         _grantRole(MINTER_ROLE, msg.sender); // Initial minter is deployer
     }
